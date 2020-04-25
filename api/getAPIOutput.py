b2f7b740177b6b2816b616d452cf652710314242
Soumya1910 <rivu10@gmail.com>
2020-04-25 15:00:01 +0530

    Covid-19 webhook response

--- api/getAPIOutput.py
+++ api/getAPIOutput.py
@@ -1,4 +1,6 @@
 from urllib.parse import quote
+from flask import jsonify
+
 errorMessage = 'Sorry! We didn\'t find any record with this input. Please try with other criteria. Thank you.'
 
 
@@ -7,13 +9,12 @@ def spaceRemoveFromParam(param):
 
 
 def stateByZipAPIURL(zip_code):
-    api_url = api_url = f'https://api.postalpincode.in/pincode/{spaceRemoveFromParam(zip_code)}'
+    api_url = api_url = f'https://api.postalpincode.in/pincode/{zip_code}'
     return api_url
 
 
 def stateByCity(city_name):
-    # api_url = f'https://api.postalpincode.in/postoffice/{spaceRemoveFromParam(city_name)}'
-    api_url = f'https://api.postalpincode.in/postoffice/{city_name}'
+    api_url = f'https://api.postalpincode.in/postoffice/{spaceRemoveFromParam(city_name)}'
     return api_url
 
 
@@ -25,13 +26,13 @@ def covidDataURL():
 def getStateByZipCode(json):
     data = json[0]
     # print(data)
-    if data['Status']!= 'Success':
+    if data['Status'] != 'Success':
         return errorMessage, None
     else:
         postoffice = data['PostOffice'][0]
         # print('postoffice : ', postoffice)
         state = postoffice['State']
-        print('State : ', state)
+        # print('State : ', state)
         return None, state
 
 
@@ -44,7 +45,7 @@ def getStateByCity(json):
         postoffice = data['PostOffice'][0]
         # print('PostOffice : ', postoffice)
         state = postoffice['State']
-        print('State : ', state)
+        # print('State : ', state)
         return None, state
 
 
@@ -52,8 +53,10 @@ def getCovidDataForState(json, state_name):
     statewise_data = json['statewise']
     if state_name is not None:
         state_record = [state for state in statewise_data if (state['state']).upper() == state_name.upper()][0]
-        string = 'State : \t' + state_name + '\nTotal Case : \t' + str(
-            state_record['confirmed']) + '\nActive Case : \t' + str(state_record['active']) + \
-                 '\nDeath : \t' + str(state_record['deaths']) + '\nRecovered : \t' + str(state_record['recovered'])
-        print(string)
-        return string
+        return responseFormat(state_record)
+
+
+def responseFormat(state_record):
+    resp = f"state : {state_record['state']}\t\ttotal case : {state_record['confirmed']}\t\tactive case : {state_record['active']}\t\tdeath : {state_record['deaths']}\t\trecovered : {state_record['recovered']}"
+    print('Final Response : ', resp)
+    return {"fulfillmentText": resp}
--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
-
+import json
 from api import getAPIOutput
 from api import getHeaderResponse
 
-state_list = ['Andra Pradesh','Arunachal Pradesh','Assam','Bihar','Chhattisgarh','Goa','Gujarat','Haryana','Himachal Pradesh',
-              'Jammu and Kashmir','Jharkhand','Karnataka','Kerala','Madya Pradesh','Maharashtra','Manipur','Meghalaya',
-              'Mizoram','Nagaland','Orissa','Punjab','Rajasthan','Sikkim','Tamil Nadu','Telagana','Tripura','Uttaranchal',
-              'Uttar Pradesh','West Bengal','Chandigarh','Delhi','Lakshadeep','Pondicherry','Andaman and Nicobar','Dadar and Nagar Haveli', 'Daman and Diu']
+state_list = ['Andra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chhattisgarh', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh',
+              'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Madya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya',
+              'Mizoram', 'Nagaland', 'Orissa', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telagana', 'Tripura', 'Uttaranchal',
+              'Uttar Pradesh', 'West Bengal', 'Chandigarh', 'Delhi', 'Lakshadeep', 'Pondicherry', 'Andaman and Nicobar', 'Dadar and Nagar Haveli', 'Daman and Diu']
 
 state_list = map(str.lower, state_list)
 app = Flask(__name__)
@@ -31,13 +31,13 @@ def covid_data(state='Telangana'):
 @app.route('/zip/<pin_code>', methods=['GET'])
 @cross_origin()
 def findByPinCode(pin_code):
-    print('Given pin code : ', pin_code)
+    # print('Given pin code : ', pin_code)
     api_url = getAPIOutput.stateByZipAPIURL(pin_code)
     resp = getHeaderResponse.response(api_url)
     errorMessage, state = getAPIOutput.getStateByZipCode(resp)
     # print('Pin code is ' + pin_code + '\t' + state)
     if state is None:
-        return errorMessage
+        return {"fulfillmentText": errorMessage}
     else:
         return covid_data(state)
 
@@ -45,7 +45,7 @@ def findByPinCode(pin_code):
 @app.route('/city/<city_name>', methods=['GET'])
 @cross_origin()
 def findByCityName(city_name):
-    print('City Name : ', city_name)
+    # print('City Name : ', city_name)
     if city_name in state_list:
         state = city_name
     else:
@@ -59,5 +59,20 @@ def findByCityName(city_name):
         return covid_data(state)
 
 
+@app.route('/covid', methods=['POST'])
+@cross_origin()
+def getCovidData():
+    req = request.get_json(silent=True, force=True)
+    intent_name = req['queryResult']['intent']['displayName']
+    if intent_name == 'pin-code':
+        pin_code = req['queryResult']['parameters']['zip_code']
+        print('Given Pin Code is : ' + pin_code)
+        return findByPinCode(pin_code)
+    elif intent_name == 'state-info':
+        city_name = req['queryResult']['parameters']['geo_city']
+        print('Given city is : ' + city_name)
+        return findByCityName(city_name)
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True)
